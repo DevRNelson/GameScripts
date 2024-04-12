f2a82e08951700154e31e039dfd89b8d5ff9acc6
nelson <rinaldo.nelson1@gmail.com>
2024-04-11 20:17:45 -0400

    Character Controller Added

--- PlayerMotor.cs
+++ PlayerMotor.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    private CharacterController
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //recieves inputs for InputManager and Applies them to Controller
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime)
     }
 }
